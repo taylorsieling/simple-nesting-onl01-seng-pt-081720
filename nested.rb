0421a5d28053c729baadeead42bce250037c140e
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 15:43:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,7 +91,8 @@ def adding_matz
         }
      }
 
- programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]  
+ programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}  
+
 end
 
 adding_matz
